29e40acd8fc1ca4458749409d59653522eb47d9a
jmayo <jcmmayo@gmail.com>
2016-06-03 00:16:29 -0500

    avance inventario 03-06-2016

---
+++ js/seleccion_tabla.js
@@ -0,0 +1,15 @@
+$(document).ready(iniciar);
+	function myFunction(x) {
+        // Obtenemos todos los TR de la tabla con id "tabla"
+        // despues del tbody.
+        var elementos = document.getElementById('tabla').
+        getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+ 
+        // Por cada TR empezando por el segundo, ponemos fondo.
+        for (var i = 1; i < elementos.length; i++) {
+            elementos[i].style.background='#dddddc';
+        }
+
+        // Al elemento clickeado le ponemos fondo.
+        x.style.background="#696868";
+    }
