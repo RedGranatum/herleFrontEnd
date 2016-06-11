2ca2c8b62e14f679e3578bdf16066f2315db8a04
ivan martinez <ivan_17_carro@hotmail.com>
2016-06-10 19:35:15 -0500

    menuuu

--- public/js/menuPrincipal.jsx
+++ public/js/menuPrincipal.jsx
@@ -12,8 +12,8 @@ module.exports  = React.createClass({
 		    <BotonMenu colorLink={"ico_nav"} icono={"group"}  tam={"3x"} ruta={"/clientes"}/>
 			<BotonMenu colorLink={"ico_nav"} icono={"shopping-cart"}  tam={"3x"} ruta={"/compras"}/>
 			<BotonMenu colorLink={"ico_nav"} icono={"dollar"}  tam={"3x"} ruta={"/inventarios"}/>
-			<BotonMenu colorLink={"ico_nav"} icono={"money"}  tam={"3x"}/>
-			<BotonMenu colorLink={"ico_nav"} icono={"info"}  tam={"3x"}/>
+			<BotonMenu colorLink={"ico_nav"} icono={"money"}  tam={"3x"} ruta={"/ventas"}/>
+			<BotonMenu colorLink={"ico_nav"} icono={"info"}  tam={"3x"} ruta={"/costos"}/>
 			<BotonMenu colorLink={"ico_nav"} icono={"th"}  tam={"3x"}/>
 			<BotonMenu colorLink={"ico_logout"} icono={"sign-out"}  tam={"2x"}/>
 		</ul>
