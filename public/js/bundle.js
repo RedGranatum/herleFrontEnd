26dee066b44a7d71188bdf21136a4899fefb03dd
raultr <raultr@gmail.com>
2016-06-18 00:37:57 -0500

    archivos para el servidor

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -2466,9 +2466,9 @@ $(function () {
 								Url: {}
 				};
 
-				var url_local = 'http://localhost:8000/';
+				//var url_local = 'http://localhost:8000/'
 				//var url_local ='http://192.168.0.15:8000/';
-				//var url_local = 'http://107.170.1.182:8000/'
+				var url_local = 'http://159.203.229.118:8000/';
 
 				datosCatalogo = new ApiRestCatalogo();
 
