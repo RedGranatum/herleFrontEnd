1a9a70b2047c124dab4f774adb4c6a230677e7ac
raultr <raultr@gmail.com>
2016-06-19 13:40:12 -0500

    cambios para el servidor

--- js/bundle.js
+++ js/bundle.js
@@ -2537,9 +2537,9 @@ $(function () {
 								Url: {}
 				};
 
-				var url_local = 'http://localhost:8000/';
+				//var url_local = 'http://localhost:8000/'
 				//var url_local ='http://192.168.0.15:8000/';
-				//var url_local = 'http://159.203.229.118/'
+				var url_local = 'http://159.203.229.118/';
 
 				datosCatalogo = new ApiRestCatalogo();
 
