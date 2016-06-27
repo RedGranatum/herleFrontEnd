f9f31b62863c0cf5c0e24b8f0345c58d7616abc0
raultr <raultr@gmail.com>
2016-06-27 04:05:02 -0500

    cabecera de ventas con busqueda del cliente

--- js/bundle.js
+++ js/bundle.js
@@ -4479,6 +4479,8 @@ var Combo = require('../js/combo.jsx');
 var AreaTexto = require('../js/areaTexto.jsx');
 var CajaConCampos = require('../js/cajaConCampos.jsx');
 var Titulo = require('../js/titulos.jsx');
+var ListaResultados = require('../js/resultadosLista.jsx');
+var ApiRestCliente = require('../js/modelos/apirestClientes');
 
 module.exports = React.createClass({
 	displayName: 'exports',
@@ -4489,46 +4491,60 @@ module.exports = React.createClass({
 	componentDidMount: function () {
 		var self = this;
 		$("#fec_inventario").datepicker({ dateFormat: "dd/mm/yy" }).on("input change", function (e) {
-			//self.cambiarValorFecha(e.target.id,e.target.value);
+			self.cambiarValorFecha(e.target.id, e.target.value);
 			console.log("Date changed: ", e.target.value);
 		});
 	},
+	cambiarValorFecha: function (control, valor) {
+		var update = {};
+		update[control] = valor;
+		this.setState(update);
+	},
 	componentWillReceiveProps: function (nextProps) {
-		// if(nextProps.datos.id === undefined){
-		var cabecero = nextProps.datos;
-		// 	var lista_nueva = this.filtrarFilasSinValidar(nextProps.datos.compra_detalles)
-		//       var detalle = this.seleccionarPrimeraFila(lista_nueva)    
+		if (nextProps.datos.id !== undefined) {
+			var cabecero = nextProps.datos;
+			var cliente_id = cabecero.cliente.id;
+			var cliente_codigo = cabecero.cliente.codigo;
+			var banco_cliente = cabecero.cliente.banco;
 
-		this.setState({
-			id: cabecero.id,
-			tipo_doc: cabecero.tipo_doc,
-			num_documento: cabecero.num_documento,
-			bln_activa: cabecero.bln_activa,
-			fec_inventario: cabecero.fec_inventario,
-			metodo_pago: cabecero.metodo_pago,
-			cantidad_pago: cabecero.cantidad_pago,
-			observaciones: cabecero.observaciones
-		});
+			var cliente_nombre = "[" + cliente_codigo + "] " + cabecero.cliente.nombre;
 
-		// 	 }
-		//  else{
-		//  	this.setState(this.getInitialState())
-		// }
+			this.setState({
+				id: cabecero.id,
+				tipo_doc: cabecero.tipo_doc,
+				num_documento: cabecero.num_documento,
+				bln_activa: cabecero.bln_activa,
+				fec_inventario: cabecero.fec_inventario,
+				cliente: cliente_id,
+				cliente_codigo: cliente_codigo,
+				cliente_nombre: cliente_nombre,
+				metodo_pago: cabecero.metodo_pago,
+				banco_cliente: banco_cliente,
+				periodo_pago: cabecero.periodo_pago,
+				cantidad_pago: cabecero.cantidad_pago,
+				observaciones: cabecero.observaciones
+			});
+		} else {
+			this.setState(this.getInitialState());
+		}
 	},
 	getInitialState: function () {
 		return {
-			"id": -1,
-			"tipo_doc": '0100000',
+			id: -1,
+			tipo_doc: '0100000',
 			num_documento: '',
-			"bln_activa": 'False',
-			"fec_inventario": moment().format('DD/MM/YYYY'),
-			"cliente": '',
-			"metodo_pago": '0110000',
-			"banco_cliente": "0030000",
-			"periodo_pago": '0120000',
-			"cantidad_pago": "0.0",
-			"observaciones": '',
-			"errores": []
+			bln_activa: 'True',
+			fec_inventario: moment().format('DD/MM/YYYY'),
+			cliente: "0",
+			cliente_codigo: "",
+			cliente_nombre: '',
+			metodo_pago: '0110000',
+			banco_cliente: "0030000",
+			periodo_pago: '0120000',
+			cantidad_pago: "0.0",
+			observaciones: '',
+			busqueda_clientes: [],
+			errores: []
 		};
 	},
 	llenarCombos: function () {
@@ -4536,15 +4552,70 @@ module.exports = React.createClass({
 		this.tipos_docs = func.llenarComboGenerico(appmvc.Datos.TIPO_DOCUMENTO);
 		this.metodos_pago = func.llenarComboGenerico(appmvc.Datos.METODO_PAGO);
 		this.periodos_pago = func.llenarComboGenerico(appmvc.Datos.PERIODO_PAGO);
+		this.bancos = func.llenarComboGenerico(appmvc.Datos.BANCOS);
 
 		var status = [{ cdu_catalogo: "True", descripcion1: "Activo" }, { cdu_catalogo: "False", descripcion1: "Cancelado" }];
 		this.STATUS = func.llenarComboGenerico(status);
 	},
-	onBlurCaja: function (campo, valor) {},
+	onBlurCaja: function (control, valor) {
+		var valVal = valor;
+		if (control === "cliente_nombre") {
+			var codigo_caja = valor.substring(1, valor.indexOf("]"));
+			var codigo_state = this.state.cliente_codigo;
+			valVal = codigo_caja;
+			if (valVal === "0") {
+				valVal = "";
+			}
+
+			if (codigo_caja !== codigo_state) {
+				valVal = "";
+				var valdef = this.getInitialState();
+				self.setState({ cliente: valdef.cliente, cliente_codigo: valdef.cliente_codigo, cliente_nombre: valdef.cliente_nombre, banco_cliente: valdef.banco_cliente });
+			}
+		}
+
+		this.validarCampoErrores(control, valVal);
+	},
 	onValorCambio: function (campo, valor) {
 		var campos = {};
 		campos[campo] = valor;
 		this.setState(campos);
+	},
+	onBuscarCliente: function (control, valor) {
+		var self = this;
+		var cliente = new ApiRestCliente();
+		funcionBusqueda = cliente.buscarClientePorValor.bind(cliente);
+		funcionBusqueda(valor, function (data) {
+			self.setState({ busqueda_clientes: data });
+		}, function (model, response, options) {
+			self.setState({ busqueda_clientes: [] });
+		});
+	},
+	BuscarClientePorPk: function (pk) {
+		var self = this;
+		var cliente = new ApiRestCliente();
+		cliente.buscarClientePorPk(pk, function (data) {
+			var id = data[0].id;
+			var codigo = data[0].codigo;
+			var nombre = "[" + codigo + "] " + data[0].nombre;
+			var banco = data[0].banco;
+			self.setState({ cliente: id, cliente_codigo: codigo, cliente_nombre: nombre, banco_cliente: banco, busqueda_clientes: [] });
+		}, function (model, response, options) {
+			var valdef = self.getInitialState();
+			self.setState({ cliente: valdef.cliente, cliente_codigo: valdef.cliente_codigo, cliente_nombre: valdef.cliente_nombre, banco_cliente: valdef.banco_cliente, busqueda_clientes: [] });
+		});
+	},
+	onClaveSeleccionada: function (pk) {
+		this.BuscarClientePorPk(pk);
+		console.log("la pk :" + pk);
+	},
+	llenarListaClientes: function (lista) {
+		return lista.length > 0 ? React.createElement(
+			'div',
+			{ className: 'caja_busqueda', ref: 'busqueda_clientes_compras' },
+			' ',
+			React.createElement(ListaResultados, { ref: 'ListaResultadosBusqueda', resultados: lista, onClaveSeleccionada: this.onClaveSeleccionada })
+		) : [];
 	},
 	render: function () {
 		var func = new FuncGenericas();
@@ -4555,12 +4626,17 @@ module.exports = React.createClass({
 		var CANTIDAD_PAGO = func.zipCol(dic1, ["cantidad_pago", "Cantidad Pago", "Cantidad Pago", this.state.cantidad_pago, this.onValorCambio, "", this.onBlurCaja, this.state.errores.num_doc]);
 		var OBSERVACIONES = func.zipCol(dic1, ["observaciones", "Observaciones", "Observaciones", this.state.observaciones, this.onValorCambio, "", this.onBlurCaja, this.state.errores.num_doc]);
 
+		var CLIENTE = func.zipCol(dic1, ["cliente_nombre", "Cliente", "Cliente", this.state.cliente_nombre, this.onValorCambio, this.onBuscarCliente, this.onBlurCaja, this.state.errores.cliente_nombre]);
+
 		var dic2 = ["id", "titulo", "children", "seleccionado", "onChange"];
 		var TIPOS_DOCUMENTOS = func.zipCol(dic2, ["tipo_doc", "Tipo de documento", this.tipos_docs, this.state.tipo_doc, this.onValorCambio]);
 		var METODOS_PAGO = func.zipCol(dic2, ["metodo_pago", "Metodo Pago", this.metodos_pago, this.state.metodo_pago, this.onValorCambio]);
 		var STATUS = func.zipCol(dic2, ["bln_activa", "Status", this.STATUS, this.state.bln_activa, this.onValorCambio]);
 		var PERIODO_PAGO = func.zipCol(dic2, ["periodo_pago", "Periodo Pago", this.periodos_pago, this.state.periodo_pago, this.onValorCambio]);
 
+		var BANCOS = func.zipCol(dic2, ["banco_cliente", "Banco", this.bancos, this.state.banco_cliente, this.onValorCambio]);
+
+		var busqueda_clientes = this.llenarListaClientes(this.state.busqueda_clientes);
 		//debugger;
 		return React.createElement(
 			'article',
@@ -4579,95 +4655,10 @@ module.exports = React.createClass({
 			React.createElement(
 				CajaConCampos,
 				{ clase: "caja_bloque" },
-				React.createElement(
-					'li',
-					{ className: 'li_bloque' },
-					React.createElement(
-						'label',
-						{ className: 'etiquetas_bloque', 'for': 'proveedor' },
-						'Cliente'
-					),
-					React.createElement('input', { className: 'inputs_bloque', type: 'text', placeholder: 'Código, Nombre' }),
-					React.createElement(
-						'div',
-						{ className: 'viñeta' },
-						'*'
-					),
-					React.createElement(
-						'div',
-						{ className: 'bloque_resultados_proveedor' },
-						React.createElement(
-							'div',
-							{ className: 'resultado_proveedor' },
-							React.createElement(
-								'span',
-								{ className: 'dsc_resultado_proveedor' },
-								'Resultado A'
-							)
-						),
-						React.createElement(
-							'div',
-							{ className: 'resultado_proveedor' },
-							React.createElement(
-								'span',
-								{ className: 'dsc_resultado_proveedor' },
-								'Resultado B'
-							)
-						),
-						React.createElement(
-							'div',
-							{ className: 'resultado_proveedor' },
-							React.createElement(
-								'span',
-								{ className: 'dsc_resultado_proveedor' },
-								'Resultado C'
-							)
-						),
-						React.createElement(
-							'div',
-							{ className: 'resultado_proveedor' },
-							React.createElement(
-								'span',
-								{ className: 'dsc_resultado_proveedor' },
-								'Resultado D'
-							)
-						)
-					),
-					React.createElement(
-						'div',
-						{ className: 'error_mostrar' },
-						'mensaje de error del campo'
-					)
-				),
+				React.createElement(CajaDeTexto, { propiedades: CLIENTE }),
+				busqueda_clientes,
 				React.createElement(Combo, { propiedades: METODOS_PAGO }),
-				React.createElement(
-					'li',
-					{ className: 'li_bloque' },
-					React.createElement(
-						'label',
-						{ className: 'etiquetas_bloque', 'for': 'invoice' },
-						'Banco'
-					),
-					React.createElement(
-						'label',
-						{ className: 'etiqueta_especial', 'for': '' },
-						'Banco cliente'
-					)
-				),
-				React.createElement(
-					'li',
-					{ className: 'li_bloque' },
-					React.createElement(
-						'label',
-						{ className: 'etiquetas_bloque', 'for': 'invoice' },
-						'Estatus'
-					),
-					React.createElement(
-						'label',
-						{ className: 'etiqueta_especial', 'for': '' },
-						'Debe, No Debe'
-					)
-				)
+				React.createElement(Combo, { propiedades: BANCOS })
 			),
 			React.createElement(
 				'div',
@@ -4687,7 +4678,7 @@ module.exports = React.createClass({
 	}
 });
 
-},{"../js/areaTexto.jsx":2,"../js/cajaConCampos.jsx":4,"../js/cajaDeTexto.jsx":6,"../js/combo.jsx":10,"../js/funcionesGenericas":18,"../js/titulos.jsx":49,"jquery":57,"moment":58,"react":219}],53:[function(require,module,exports){
+},{"../js/areaTexto.jsx":2,"../js/cajaConCampos.jsx":4,"../js/cajaDeTexto.jsx":6,"../js/combo.jsx":10,"../js/funcionesGenericas":18,"../js/modelos/apirestClientes":31,"../js/resultadosLista.jsx":47,"../js/titulos.jsx":49,"jquery":57,"moment":58,"react":219}],53:[function(require,module,exports){
 
 var React = require('react');
 
