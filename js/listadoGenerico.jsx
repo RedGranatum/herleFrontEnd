76ae617466672a2ae4b88076729f454fa4492487
raultr <raultr@gmail.com>
2016-07-08 00:14:44 -0500

    primer reporte generico (inventario)

--- js/listadoGenerico.jsx
+++ js/listadoGenerico.jsx
@@ -7,35 +7,48 @@ getDefaultProps: function(){
 		estilo: 'block',
 		titulos: {},
 		datos: [],
+		id: 'Id',
 	}
 },
-componentWillMount: function(){
-	this.listadoFilas = [];
-},
-componentDidMount: function(){
-	this.crearListado();
-},
-llenarFila: function(diccionario){
+llenarFila: function(diccionario, num_fila){
+	
+	estilo={}
+    estilo["background"]="#dddddc";
+        
+    if(num_fila % 2 === 0){
+        estilo["background"] = "#FFFFFF";
+    }       
+    if(num_fila === 'titulo'){
+        estilo["background"] = "#b08f88";
+    }
+	
 	var filaInd = []
-   	
-	Object.keys(this.props.titulos).forEach(function (titulo) {
-		 filaInd.push(<td contentEditable={false} > {diccionario[titulo]} </td>);
+
+ 	Object.keys(this.props.titulos).forEach(function (titulo) {
+ 		 filaInd.push(<td key={titulo} contentEditable={false} style={estilo}> {diccionario[titulo]} </td>);
    });
-   		this.listadoFilas.push(<tr  style={estilo}> {filaInd} </tr>);
+   		this.listadoFilas.push(<tr  key={num_fila}> {filaInd} </tr>);
 },
 llenarTitulos: function(){
-	this.llenarFila(this.props.titulos);
-}
+	this.llenarFila(this.props.titulos,'titulo');
+},
 llenarFilaDatos: function(){
+    var self = this;
+    var num_fila = 1;
 	this.props.datos.forEach(function(datos){
-         llenarFila(datos);
+         self.llenarFila(datos, num_fila);
+         num_fila+=1;
 	});
 },
 crearListado: function(){
+	this.listadoFilas = [];
 	this.llenarTitulos();
     this.llenarFilaDatos();
 },
+
 render: function () {
+     this.crearListado();
+
      return (      		
            	<table className="tabla_catalogo">
 				<tbody>
--- js/reportes.jsx
+++ js/reportes.jsx
@@ -4,7 +4,7 @@ var CeldaTabla       = require('../js/celdaTabla.jsx');
 var FilaTabla        = require('../js/filaTabla.jsx');
 var ApiRestInventario = require('../js/modelos/apirestInventarios');
 var ApiRestExistencias = require('../js/modelos/apirestExistencias');
-
+var ListadoGenerico    = require('../js/listadoGenerico.jsx');
 
 module.exports = React.createClass({
 getDefaultProps: function(){
@@ -14,90 +14,34 @@ getDefaultProps: function(){
 },
 getInitialState: function(){
 	return {
-		lista_inventario: []
+		columna_id: 'Id',
+		lista_datos: [],
+		titulos_encabezado: {}
 	}
 },
 onClickReporte: function(){
 	console.log("Reporte seleccionado")
 	this.llenarListaExistencias();
 },
-// llenarListaClientes: function(){
-// 	var self = this;
-// 	var Inventario = new ApiRestInventario();
-// 	Inventario.listadoInventario(	
-// 		function(data){
-//    				self.setState({lista_inventario: data });
-// 		},
-// 		function(model,response,options){
-// 				    self.setState({lista_inventario : [] });
-// 		}
-// 	);
-// },
 llenarListaExistencias: function(){
 	var self = this;
+	var titulosEncabezado={num_rollo:"Num.Rollo",codigo_producto: "Producto",calibre:"Milesimas",ancho:"Ancho",entradas_kg:"Entradas Kg",salidas_kg:"Salida Kg",existencia_kg:"Existencias Kg"};
+
 	var existencias = new ApiRestExistencias();
 	existencias.buscarExistenciaAgrupadasPorRollo(	
 		function(data){
-   				self.setState({lista_inventario: data });
+   				self.setState({lista_datos: data, titulos_encabezado: titulosEncabezado, columna_id:"num_rollo" });
 		},
 		function(model,response,options){
-				    self.setState({lista_inventario : [] });
+				    self.setState({lista_datos : [] , titulos_encabezado: titulosEncabezado, columna_id:"num_rollo" });
 		}
 	);
 },
 render: function () {
-
-	  // producto1 =  {"id": 17,"compra_detalle": 9,"num_rollo": "SD2","codigo_producto": "PAQUETERIA12",
-   //      "calibre": "3.000","ancho": "4.00","largo": 12,"peso_kg": "22.67965","peso_lb": "50.00001"}
-
-	  // producto2 =  {"id": 18,"compra_detalle": 10,"num_rollo": "AA33","codigo_producto": "CP11",
-   //      "calibre": "13.000","ancho": "14.00","largo": 11,"peso_kg": "12.67965","peso_lb": "150.00001"}
-      
-   //    lista_productos = []
-   //    lista_productos.push(producto1);
-   //    lista_productos.push(producto2);
-	
 	var self= this;
 	var estilo = {cursor:"pointer"};
 
-	//var titulosEncabezado=["Id", "Num.Rollo","Codigo Producto",	"Calibre",	"Ancho",	"Largo",	"Peso (Kgs)",	"Peso (Lbs)"];
-	var titulosEncabezado=[ "Num.Rollo","Producto","Milesimas","Ancho","Entradas Kg",	"Salida Kg",	"Existencias Kg"];
-
-
-	var encabezado=titulosEncabezado.map(function(titulo){
-     return (
-           <CeldaTabla key={titulo} contenido={titulo} />
-     	);
-	});
-
-	var listado_detalles = [];
-	var i=1;
-	this.state.lista_inventario.forEach(function(resultado){
-		var detalle = []
-
-			// detalle.push(<CeldaTabla contenido={resultado.id} key="id"/>);
-			// detalle.push(<CeldaTabla contenido={resultado.num_rollo} key="num_rollo" />);
-			// detalle.push(<CeldaTabla contenido={resultado.codigo_producto}  key="codigo_producto" />);
-			// detalle.push(<CeldaTabla contenido={resultado.calibre}  key="calibre" />);
-			// detalle.push(<CeldaTabla contenido={resultado.ancho}  key="ancho" />);
-			// detalle.push(<CeldaTabla contenido={resultado.largo}  key="largo"/>);
-			// detalle.push(<CeldaTabla contenido={resultado.peso_kg}  key="peso_kg" />);
-			// detalle.push(<CeldaTabla contenido={resultado.peso_lb} key="peso_lb" />);
-	
-		 //    listado_detalles.push(<FilaTabla key={resultado.id} id={resultado.id} childrens={detalle} num_fila={i} estilo={estilo} onSeleccionFila={self.onSeleccionFila}/>)
-	 		detalle.push(<CeldaTabla contenido={resultado.num_rollo} key="num_rollo"/>);
-	 		detalle.push(<CeldaTabla contenido={resultado.codigo_producto} key="codigo_producto"/>);
-
-	 		detalle.push(<CeldaTabla contenido={resultado.calibre} key="calibre_producto"/>);
-	 		detalle.push(<CeldaTabla contenido={resultado.ancho} key="ancho_producto"/>);
-
-			detalle.push(<CeldaTabla contenido={resultado.entradas_kg} key="entradas_kg" />);
-			detalle.push(<CeldaTabla contenido={resultado.salidas_kg}  key="salidas_kg" />);
-			detalle.push(<CeldaTabla contenido={resultado.existencia_kg}  key="existencia_kg" />);
-		
-		     listado_detalles.push(<FilaTabla key={resultado.num_rollo} id={resultado.num_rollo} childrens={detalle} num_fila={i} estilo={estilo} onSeleccionFila={self.onSeleccionFila}/>)
-	   		i=i+1;
-	})
+ 
 
 	  return (      		
 			<section className="contenido">
@@ -107,12 +51,7 @@ render: function () {
 				<article className="bloque">
 					<div className="caja_bloque">
 						<div className="bloque_catalogo" id="ampliar_tabla">
-							<table className="tabla_catalogo">
-								<tbody>
-								<FilaTabla childrens={encabezado}/>
-								{listado_detalles}
-								</tbody>
-							</table>
+							<ListadoGenerico id={this.state.columna_id} titulos={this.state.titulos_encabezado} datos={this.state.lista_datos}/>
 						</div>
 					</div>
 				</article>
