9c9fd554b69bcef371e1f648491ad279ab3d8cf3
raultr <raultr@gmail.com>
2016-08-17 19:29:31 -0500

    sin permisos

--- js/app.jsx
+++ js/app.jsx
@@ -38,7 +38,7 @@ module.exports = React.createClass({
           datosCostos: {},
           datosPagos: {},
           actualizarForm: false,
-          permiso: true,
+          permiso: false,
 
      		};
 	 	},
