c3f6565c30452ef499e52bf595d5b97cf081fe05
raultr <raultr@gmail.com>
2016-09-06 00:13:32 -0500

    Nota de venta con pie de pagina

--- js/ventas.jsx
+++ js/ventas.jsx
@@ -31,13 +31,14 @@ nuevosDatos: function(){
   },
 		render: function () {         
        	   var venta_detalles = (this.props.datos.venta_detalles === undefined) ? [] : this.props.datos.venta_detalles;
-		   
+		 
 		   return(  
 		   	<div >
 		       	
 		        <VentasCabecero datos={this.props.datos} ref="cabecero_ventas" />
 				  
 		         <VentasListado 
+ 		                activa ={this.props.datos.bln_activa}
 		                listado ={venta_detalles} 
 		                id_venta = {this.props.datos.id} 
 		                ref="listado_detalles_ventas" 
