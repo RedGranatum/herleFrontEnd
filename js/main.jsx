3d2841f39601fb6ee44b93581ba2be4bef432908
raultr <raultr@gmail.com>
2017-11-25 01:23:42 -0600

    ruta del servidor en la nube

--- js/main.jsx
+++ js/main.jsx
@@ -21,10 +21,11 @@ $(function() {
  
   //var url_local ='http://192.168.0.15:8000/';
   //var url_local = 'http://192.241.197.67/herleapp/'
-	var url_local = 'http://localhost:8000/'
-  //var url_local = 'http://159.203.229.118/'
+	//var url_local = 'http://localhost:8000/'
+  var url_local = 'http://159.203.229.118/'
 //ssh -p 2234 miregion@104.131.161.180
 //ssh herleuser@159.203.229.118
+ //var url_local = 'http://159.203.229.118/'
 
 
 
